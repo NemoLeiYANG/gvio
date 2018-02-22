570ec344fac4ebc8e50d92c6ee6c7393bd18f062
Chris Choi <chutsu@gmail.com>
2018-02-22 18:22:53 -0500

    Add kalibr install script

---
+++ scripts/install/kalibr_install.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+ROS_VERSION=kinetic
+CATKIN_WS=$HOME/kalibr_ws
+
+sudo apt-get install -qq -y \
+    python-setuptools \
+    python-rosinstall \
+    ipython \
+    libeigen3-dev \
+    libboost-all-dev \
+    doxygen \
+    libopencv-dev \
+    python-software-properties \
+    software-properties-common \
+    libpoco-dev \
+    python-matplotlib \
+    python-scipy \
+    python-git \
+    python-pip \
+    ipython libtbb-dev \
+    libblas-dev \
+    liblapack-dev \
+    python-catkin-tools \
+    libv4l-dev \
+    ros-$ROS_VERSION-vision-opencv \
+    ros-$ROS_VERSION-image-transport-plugins \
+    ros-$ROS_VERSION-cmake-modules
+
+sudo pip install python-igraph --upgrade
+
+# Create Kalibr catkin workspace
+mkdir -p $CATKIN_WS/src
+cd $CATKIN_WS
+catkin init
+
+# Clone Kalibr
+cd src
+if [ ! -d Kalibr ]; then
+    git clone https://github.com/ethz-asl/Kalibr.git
+fi
+
+# Build Kalibr
+cd $CATKIN_WS
+catkin build
